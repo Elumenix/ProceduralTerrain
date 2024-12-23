5513dd68373db70b2b91b1903c2971534fdf69ea
Elumenix <dps5393@g.rit.edu>
2024-12-23 14:12:51 -0500

    Lighting and antialiasing added to raycasting
    Lighting seems to have broken some things

--- Assets/Scripts/RayTracingMaster.cs
+++ Assets/Scripts/RayTracingMaster.cs
@@ -1,25 +1,48 @@
-using System;
 using UnityEngine;
-using UnityEngine.Experimental.Rendering;
+using Random = UnityEngine.Random;
 
 [ExecuteInEditMode]
 public class RayTracingMaster : MonoBehaviour
 {
     public ComputeShader RayTracingShader;
     public Texture SkyboxTexture;
+    public Light DirectionalLight;
+    public int NumReflections = 8;
     private RenderTexture _target;
+    private uint _currentSample = 0;
+    private Material _addMaterial;
+    private int prevNumReflections = 8;
     
     // Cached strings
     private static readonly int Result = Shader.PropertyToID("Result");
     private static readonly int CameraToWorld = Shader.PropertyToID("_CameraToWorld");
     private static readonly int CameraInverseProjection = Shader.PropertyToID("_CameraInverseProjection");
     private static readonly int SkyboxTexture1 = Shader.PropertyToID("_SkyboxTexture");
+    private static readonly int Sample = Shader.PropertyToID("_Sample");
+    private static readonly int PixelOffset = Shader.PropertyToID("_PixelOffset");
+    private static readonly int Reflections = Shader.PropertyToID("numReflections");
+    private static readonly int DirectionalLight1 = Shader.PropertyToID("_DirectionalLight");
 
+
+    private void Update()
+    {
+        if (!transform.hasChanged || DirectionalLight.transform.hasChanged || prevNumReflections != NumReflections) return;
+
+        _currentSample = 0;
+        transform.hasChanged = false;
+        DirectionalLight.transform.hasChanged = false;
+        prevNumReflections = NumReflections;
+    }
+    
     private void SetShaderParameters()
     {
         RayTracingShader.SetMatrix(CameraToWorld, Camera.current.cameraToWorldMatrix);
         RayTracingShader.SetMatrix(CameraInverseProjection, Camera.current.projectionMatrix.inverse);
         RayTracingShader.SetTexture(0, SkyboxTexture1, SkyboxTexture);
+        RayTracingShader.SetVector(PixelOffset, new Vector2(Random.value, Random.value));
+        Vector3 l = DirectionalLight.transform.forward;
+        RayTracingShader.SetVector(DirectionalLight1, new Vector4(l.x, l.z, DirectionalLight.intensity));
+        RayTracingShader.SetInt(Reflections, NumReflections);
     }
     
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
@@ -38,8 +61,16 @@ public class RayTracingMaster : MonoBehaviour
         int threadGroupsY = Mathf.CeilToInt(Screen.height / 8.0f);
         RayTracingShader.Dispatch(0, threadGroupsX, threadGroupsY, 1);
         
+        // Anti-aliasing
+        if (_addMaterial == null)
+        {
+            _addMaterial = new Material(Shader.Find("Hidden/AddShader"));
+        } 
+        _addMaterial.SetFloat(Sample, _currentSample);
+        
         // Pass texture to render target / screen
-        Graphics.Blit(_target, destination);
+        Graphics.Blit(_target, destination, _addMaterial);
+        _currentSample++;
     }
 
     private void InitRenderTexture()
@@ -51,7 +82,7 @@ public class RayTracingMaster : MonoBehaviour
         {
             _target.Release();
         }
-                
+        
         // Get new render target
         _target = new RenderTexture(Screen.width, Screen.height, 0,
             RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear)
@@ -60,5 +91,6 @@ public class RayTracingMaster : MonoBehaviour
         };
                 
         _target.Create();
+        _currentSample = 0;
     }
 }
